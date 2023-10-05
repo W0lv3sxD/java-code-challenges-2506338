990c0c19d30c2cb5aa46c991b727f506ebd1de31
W0lv3sxD <93610323+W0lv3sxD@users.noreply.github.com>
2023-10-05 13:20:42 +0000

    - Added HappyNumbers - Added HappyNumberTest

---
+++ src/main/java/com/linkedinlearning/challenges/HappyNumbers.java
@@ -0,0 +1,27 @@
+package com.linkedinlearning.challenges;
+
+public class HappyNumbers {
+
+    public static void main(String[] args) {
+        System.out.println(isHappy(69));
+    }
+
+    public static boolean isHappy(int n) {
+        int slow = n, fast = n;
+        do {
+            slow = digitSquareSum(slow);
+            fast = digitSquareSum(digitSquareSum(fast));
+        } while (slow != fast);
+        return slow == 1;
+    }
+
+    private static int digitSquareSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
