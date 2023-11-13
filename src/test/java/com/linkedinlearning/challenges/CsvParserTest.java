4abccab730bda1a9458ffc95dfbd8010aa8081ac
W0lv3sxD <93610323+W0lv3sxD@users.noreply.github.com>
2023-11-13 14:16:07 +0000

    - Added

---
+++ src/test/java/com/linkedinlearning/challenges/CsvParserTest.java
@@ -0,0 +1,20 @@
+package com.linkedinlearning.challenges;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CsvParserTest {
+    
+    @Test
+    void testParse() throws IOException {
+        var csvParser = new CsvParser();
+        Map<Integer, String> turingAwardWinners = csvParser.parse("src/test/resources/turing.csv");
+        
+        System.out.println(turingAwardWinners);
+        Assertions.assertEquals("Richard Hamming", turingAwardWinners.get(1968));
+        Assertions.assertEquals("Allen Newell, Herbert A. Simon", turingAwardWinners.get(1975));
+    }
+}
