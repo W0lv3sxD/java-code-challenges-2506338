6af7ebe90b976cb2f3361f2ba242a6e6c8758a48
W0lv3sxD <93610323+W0lv3sxD@users.noreply.github.com>
2023-11-13 14:18:08 +0000

    - Added CsvParser.java

---
+++ src/main/java/com/linkedinlearning/challenges/CsvParser.java
@@ -0,0 +1,29 @@
+package com.linkedinlearning.challenges;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CsvParser {
+
+    public Map<Integer, String> parse(String fileName) {
+
+        Map<Integer, String> turingAwardWinner = new HashMap<>();
+
+        try (FileReader fileReader = new FileReader(fileName);
+                BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            turingAwardWinner = bufferedReader.lines()
+                .map(s -> s.split(","))
+                .collect(Collectors.toMap(
+                    s -> Integer.parseInt(s[0]),
+                    s -> s[1], (winner1, winner2) -> winner1 + ", " + winner2));
+        } catch (IOException e) {
+            System.out.println("Error reading file '" + fileName + "'");
+        }
+
+        return turingAwardWinner;
+    }
+}
